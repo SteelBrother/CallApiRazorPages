2d2ff45bbe33e408c7d788e6077aa348500f742a
SteelBrother <nsmoreno.738@gmail.com>
2021-08-09 19:49:19 -0500

    PrimerCommit

---
+++ CallingApiWithRazor/Models/TopGamesModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CallingApiWithRazor.Models
+{
+    public class TopGamesModel
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public DateTime PublicationDate { get; set; }
+    }
+}
